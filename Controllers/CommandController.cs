209b7d468583466e2806a99a33594a0db20b9d8c
Dickson Henrique Justino Daniel <ckrique@hotmail.com>
2022-07-12 13:28:53 -0300

    Ajuste em implementação do Exaustor
    Ajuste em implementação do Exaustor, ainda precisando publicar um serviço http acessível ao Conainer do Orion Broker.

---
+++ Controllers/CommandController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiExaustor.Controllers
+{
+    public class CommandController : ApiController
+    {
+        // GET: api/Start
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Start/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Start
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // Patch: api/Start
+        public void Patch([FromBody]string value)
+        {
+        }
+
+        //// POST: api/Start
+        //public void Post()
+        //{
+        //}
+
+        // PUT: api/Start/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Start/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
